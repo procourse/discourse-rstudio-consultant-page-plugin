c44ffcc46bbfdda33ae136244110d61f8ad4f9ab
Sudaraka Jayathilaka <sudarakayasindu@gmail.com>
2018-09-08 22:33:08 +0530

    FEATURE: Remove custom scope

--- plugin.rb
+++ plugin.rb
@@ -23,79 +23,14 @@ after_initialize do
 		end
 	}
 
-	class ::User
-	  scope :filter_by_username_or_email, ->(filter) do
-			user_id = UserCustomField.where('lower(value) ILIKE ? and name IN (?)', "%#{filter}%", ["user_field_1","user_field_2"]).pluck(:user_id).uniq
-			if user_id.any?
-				return where(id: user_id)
-			else
-				where('username_lower ILIKE ?', "%#{filter}%")
-			end
-		end
-	end
-
-	# class ::GroupsController
-	# 	module DiscourseRStudio
-	# 		def members
-	# 			group = find_group(:group_id)
-	#
-	# 			limit = (params[:limit] || 20).to_i
-	# 			offset = params[:offset].to_i
-	#
-	# 			if limit < 0
-	# 				raise Discourse::InvalidParameters.new(:limit)
-	# 			end
-	#
-	# 			if offset < 0
-	# 				raise Discourse::InvalidParameters.new(:offset)
-	# 			end
-	#
-	# 			dir = (params[:desc] && !params[:desc].blank?) ? 'DESC' : 'ASC'
-	# 			order = ""
-	#
-	# 			if params[:order] && %w{last_posted_at last_seen_at}.include?(params[:order])
-	# 				order = "#{params[:order]} #{dir} NULLS LAST"
-	# 			end
-	#
-	# 			users = group.users.human_users
-	# 			total = users.count
-	#
-	# 			if (filter = params[:filter]).present?
-	# 				filter = filter.split(',') if filter.include?(',')
-	#
-	# 				if current_user&.admin
-	# 					# users = users.filter_by_username_or_email(filter)
-	# 					users = users.filter_by_username_or_company(filter)
-	#
-	# 				else
-	# 					# users = users.filter_by_username(filter)
-	# 					users = users.filter_by_username_or_company(filter)
-	# 				end
-	# 			end
-	#
-	# 			members = users
-	# 				.order('NOT group_users.owner')
-	# 				.order(order)
-	# 				.order(username_lower: dir)
-	# 				.limit(limit)
-	# 				.offset(offset)
-	#
-	# 			owners = users
-	# 				.order(order)
-	# 				.order(username_lower: dir)
-	# 				.where('group_users.owner')
-	#
-	# 			render json: {
-	# 				members: serialize_data(members, GroupUserSerializer),
-	# 				owners: serialize_data(owners, GroupUserSerializer),
-	# 				meta: {
-	# 					total: total,
-	# 					limit: limit,
-	# 					offset: offset
-	# 				}
-	# 			}
+	# class ::User
+	#   scope :filter_by_username_or_email, ->(filter) do
+	# 		user_id = UserCustomField.where('lower(value) ILIKE ? and name IN (?)', "%#{filter}%", ["user_field_1","user_field_2"]).pluck(:user_id).uniq
+	# 		if user_id.any?
+	# 			return where(id: user_id)
+	# 		else
+	# 			return where('username_lower ILIKE ?', "%#{filter}%")
 	# 		end
 	# 	end
-	# 	prepend DiscourseRStudio
 	# end
 end
